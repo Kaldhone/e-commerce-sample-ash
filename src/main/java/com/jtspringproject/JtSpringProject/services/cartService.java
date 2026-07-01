5587324596ce1c051dde6486a35374defdd1f759
Ashwini S K (BLR GSS) <ashwinisk@nousinfo.com>
2026-07-01 15:42:24 +0530

    [SCRUM-3] rename cartDao to shoppingCartDao; add unit tests

--- src/main/java/com/jtspringproject/JtSpringProject/services/cartService.java
+++ src/main/java/com/jtspringproject/JtSpringProject/services/cartService.java
@@ -2,7 +2,7 @@ package com.jtspringproject.JtSpringProject.services;
 
 import java.util.List;
 
-import com.jtspringproject.JtSpringProject.dao.cartDao;
+import com.jtspringproject.JtSpringProject.dao.shoppingCartDao;
 import com.jtspringproject.JtSpringProject.models.Cart;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +10,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class cartService {
-    private final cartDao cartDao;
+    private final shoppingCartDao cartDao;
 
     @Autowired
-    public cartService(cartDao cartDao) {
+    public cartService(shoppingCartDao cartDao) {
         this.cartDao = cartDao;
     }
 
